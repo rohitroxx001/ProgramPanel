ab9f64ba4344511155a0ec6547c5e6bad4b9158f
Sorn Maksumic <maksumic@protonmail.com>
2023-06-04 12:11:13 +0200

    Create API application with Spring Boot

---
+++ presentation/api/src/test/java/com/programpanel/presentation/api/ApiApplicationTest.java
@@ -0,0 +1,12 @@
+package com.programpanel.presentation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiApplicationTest {
+    @Test
+    public void testContextLoads() {
+        // Nothing to do.
+    }
+}
